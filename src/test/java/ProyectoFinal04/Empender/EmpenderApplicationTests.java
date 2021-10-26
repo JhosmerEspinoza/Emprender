5cc73191cdf72a261670d234a80733c058a0e8cb
Jose Espinoza <jhosmer2601@hotmail.com>
2021-10-26 21:20:46 -0200

    Emprender V1

---
+++ src/test/java/ProyectoFinal04/Empender/EmpenderApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoFinal04.Empender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
